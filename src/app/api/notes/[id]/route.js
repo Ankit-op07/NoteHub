8f157974c252c912b9d3346a562775f222a0f5e6
Ankit Nagar <ankitnagar645@gmail.com>
2025-07-03 23:54:44 +0530

    deployment fixes

--- src/app/api/notes/[id]/route.js
+++ src/app/api/notes/[id]/route.js
@@ -14,7 +14,7 @@ async function connectToDB() {
 const s3Client = new S3Client({
     region: process.env.AWS_REGION,
     credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY,
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
         secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
     },
 });
